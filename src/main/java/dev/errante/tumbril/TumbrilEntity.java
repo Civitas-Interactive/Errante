dec36e5f9a72f2ca4fbae1a8a2726ce327e56f2c
Monneky <Monneky@altosNorte.mx>
2026-09-18 20:32:28 -0600

    We made Java to generate the unimplemented methods.

--- src/main/java/dev/errante/tumbril/TumbrilEntity.java
+++ src/main/java/dev/errante/tumbril/TumbrilEntity.java
@@ -1,15 +1,37 @@
 package dev.errante.tumbril;
 
+import net.minecraft.network.syncher.SynchedEntityData.Builder;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 public class TumbrilEntity extends Entity {
-    private double followDistance = 2.0;
-
     public TumbrilEntity(EntityType<? extends TumbrilEntity> entityType, Level level) {
         super(entityType, level);
     }
+
+	@Override
+	protected void addAdditionalSaveData(ValueOutput output) {
+	}
+
+	@Override
+	protected void defineSynchedData(Builder entityData) {
+	}
+
+	@Override
+	public boolean hurtServer(ServerLevel level, DamageSource source, float damage) {
+		return false;
+	}
+
+	@Override
+	protected void readAdditionalSaveData(ValueInput input) {
+	}
+
+	private double followDistance = 2.0;
 
     public void setFollowDistance(double newDistance) {
         if(newDistance <= 0){
